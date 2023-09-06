21d7f696886c9d85f2b8c31f5ea43737279788d5
venkatgajendra <143200577+venkatgajendra@users.noreply.github.com>
2023-09-06 18:56:51 +0530

    Assignment1-1.py

--- Assignment1-1.py
+++ Assignment1-1.py
@@ -1,5 +1,5 @@
-# n = int(input("enter no of Fibonacci  numbers needed "))
-n=50
+n = int(input("enter no of Fibonacci  numbers needed "))
+
 num1 = 0
 num2 = 1
 next_number = num2
@@ -9,3 +9,13 @@ while next_number <= n:
 	num1, num2 = num2, next_number
 	next_number = num1 + num2
 print()
+#  expected output
+# enter no of Fibonacci  numbers needed 50
+# 1 
+# 2 
+# 3 
+# 5 
+# 8 
+# 13 
+# 21 
+# 34 
