20594bb8972f2c21de9b1685a11dde8481c84c7b
venkatgajendra <143200577+venkatgajendra@users.noreply.github.com>
2023-09-16 15:26:28 +0530

    Create Assignment3.2.py

---
+++ Assignment3.2.py
@@ -0,0 +1,14 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+input_string = input("Enter a string: ")
+
+reversed_string = reverse_string(input_string)
+
+print("Reversed string:", reversed_string)
+"""
+expeted output 
+Enter a string: Hello, World!
+Reversed string: !dlroW ,olleH
+
+"""
